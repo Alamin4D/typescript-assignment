412f0c4131fc89847375093c2e15067efcd68401
Alamin4D <alaminahmed79000@gmail.com>
2026-05-06 23:31:26 +0600

    add reverseString utility function

--- solutions.ts
+++ solutions.ts
@@ -3,7 +3,7 @@ function filterEvenNumbers(numbers: number[]): number[] {
     return numbers.filter((num) => num % 2 === 0);
 }
 
-console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
+// console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
 
 //? Problem 2:
 function reverseString(text: string): string {
